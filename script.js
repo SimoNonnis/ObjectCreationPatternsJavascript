93f5521358db5abc9c59da182af3a92a1eed53d9
Simon Nonnis <simononnis@gmail.com>
2016-02-06 17:26:19 +0100

    Change npm keywords

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+//Factory Pattern
+(function () {
+  //Observer
+  const producer = () => {
+    const listeners = [];
+
+    const add = (listener) => listeners.push(listener)
+
+    const remove = (listener) => {
+      const index = listeners.indexOf(listener);
+      listeners.splice(index, 1);
+    };
+
+    const notify = (msg) => listeners.forEach((listener) => listener.update(msg))
+
+    return {
+        add: add,
+        remove: remove,
+        notify: notify
+    }
+  };
+
+
+  //listeners
+  const listener1 = {
+    update: (msg) => console.log('Listener1--factory message received:', msg)
+  };
+
+  const listener2 = {
+    update: (msg) => console.log('Listener2--factory message received:', msg)
+  };
+
+  const observer = producer();
+  observer.add(listener1);
+  observer.add(listener2);
+
+  observer.notify('Hey Guys!');
+
+})();
+
+
+
+
 // Prototype Pattern
 (function () {
   //Observer
@@ -43,50 +86,6 @@
 
 })();
 
-
-
-
-
-
-//Factory Pattern
-(function () {
-  //Observer
-  const producer = () => {
-    const listeners = [];
-
-    const add = (listener) => listeners.push(listener)
-
-    const remove = (listener) => {
-      const index = listeners.indexOf(listener);
-      listeners.splice(index, 1);
-    };
-
-    const notify = (msg) => listeners.forEach((listener) => listener.update(msg))
-
-    return {
-        add: add,
-        remove: remove,
-        notify: notify
-    }
-  };
-
-
-  //listeners
-  const listener1 = {
-    update: (msg) => console.log('Listener1--factory message received:', msg)
-  };
-
-  const listener2 = {
-    update: (msg) => console.log('Listener2--factory message received:', msg)
-  };
-
-  const observer = producer();
-  observer.add(listener1);
-  observer.add(listener2);
-
-  observer.notify('Hey Guys!');
-
-})();
 
 
 
